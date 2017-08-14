e0734a1cf4a6eb37533433e10454b27c5e0a5f9d
roguesir <li_xu0214@163.com>
2017-08-14 22:59:35 +0800

    SVM

---
+++ machine_learning/t_svm.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+from sklearn import svm
+import numpy as np
+
+iris = datasets.load_iris()
+print(iris.data)
+print(iris.target)
+test = np.array([3.2,3.2,1.,0.9]).reshape(1,-1)
+clf = svm.SVC(gamma=0.001, C=100.) 
+clf = clf.fit(iris.data,iris.target)
+predicted = clf.predict(test)
+print(predicted)
