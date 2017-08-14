937084199b58c8f72170103b7bb83aed4dbf119e
roguesir <li_xu0214@163.com>
2017-08-14 22:55:31 +0800

    Linear regression

--- machine_learning/t_linear_regression.py
+++ machine_learning/t_linear_regression.py
@@ -2,12 +2,15 @@
 Author:roguesir
 Github:https://github.com/roguesir/Python-practice/tree/master/machine_learning
 '''
-
+import numpy as np
 from sklearn import linear_model
 
-x_train = input_variables_value
-y_train = 
-x_test = 
+
+
+x_train = np.array([2001,2002,2003,2004,2005,2006,2007,2008,2009,2010])
+y_train = np.array([202,244,305,382,484,591,712,853,894,1013])
+
+x_test = np.array([2011])
 
 linear = linear_model.LinearRegression()
 linear.fit(x_train,y_train)
