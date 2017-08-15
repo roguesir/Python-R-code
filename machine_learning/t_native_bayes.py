7ed6375a38aeb92bec8c4b06b126acd808375999
roguesir <li_xu0214@163.com>
2017-08-15 22:36:37 +0800

    naive bayes

--- machine_learning/t_native_bayes.py
+++ machine_learning/t_native_bayes.py
@@ -5,7 +5,6 @@ import numpy as np
 iris = datasets.load_iris()
 print(iris.data)
 print(iris.target)
-
 test = np.array([3.2,3.2,1.,0.9]).reshape(1,-1)
 model = GaussianNB() 
 model = model.fit(iris.data,iris.target)
