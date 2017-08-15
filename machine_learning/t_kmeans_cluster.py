aac40824747d136f403baf61d987384a1030f653
roguesir <li_xu0214@163.com>
2017-08-15 22:36:13 +0800

    k-means cluster

--- machine_learning/t_kmeans_cluster.py
+++ machine_learning/t_kmeans_cluster.py
@@ -1,4 +1,5 @@
-import numpy as np  
+import numpy as np 
+ 
 x1 = np.array([1, 2, 3, 1, 5, 6, 5, 5, 6, 7, 8, 9, 9])  
 x2 = np.array([1, 3, 2, 2, 8, 6, 7, 6, 7, 1, 2, 1, 3])  
 x = np.array(list(zip(x1, x2))).reshape(len(x1), 2)  
