e1f27b790967ba41011576d040e9aef53ed9c7ba
roguesir <li_xu0214@163.com>
2017-09-03 17:41:16 +0800

    SVM

--- machine_learning/t_svm.py
+++ machine_learning/t_svm.py
@@ -1,3 +1,12 @@
+# coding:utf-8
+
+"""
+Author: roguesir
+Date: 2017/8/30
+GitHub: https://roguesir.github.com
+Blog: http://blog.csdn.net/roguesir
+"""
+
 from sklearn import datasets
 from sklearn import svm
 import numpy as np
