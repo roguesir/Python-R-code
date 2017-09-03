98c0e5c6288e38da6f0728db201c7101403b5320
roguesir <li_xu0214@163.com>
2017-09-03 17:37:21 +0800

    Logistic regression

--- machine_learning/t_logistic_regression.py
+++ machine_learning/t_logistic_regression.py
@@ -1,4 +1,12 @@
-# -*- coding: utf-8 -*-  
+# coding:utf-8
+
+"""
+Author: roguesir
+Date: 2017/8/30
+GitHub: https://roguesir.github.com
+Blog: http://blog.csdn.net/roguesir
+"""
+
 from matplotlib import pyplot  
 import scipy as sp  
 import numpy as np  
