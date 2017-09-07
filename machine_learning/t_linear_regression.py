c8df9277b47ed5085a442da947d1d6eaeb72f98e
roguesir <li_xu0214@163.com>
2017-09-07 22:05:25 +0800

    lr

--- machine_learning/t_linear_regression.py
+++ machine_learning/t_linear_regression.py
@@ -6,22 +6,26 @@ Date: 2017/8/30
 GitHub: https://roguesir.github.com
 Blog: http://blog.csdn.net/roguesir
 """
-
-import numpy as np
-from sklearn import linear_model
-
-
-x_train = [[2001],[2002],[2003],[2004],[2005],[2006],[2007],[2008],[2009],[2010]]
-y_train = [[202],[244],[305],[382],[484],[591],[712],[853],[894],[1013]]
-
-x_test = [[2011]]
-
-linear = linear_model.LinearRegression()
-linear.fit(x_train,y_train)
-linear.score(x_train,y_train)
-
-print('Coefficient: ',linear.coef_)
-print('Intercept: ',linear.intercept_)
-
-predicted = linear.predict(x_test)
-print(predicted)
+import numpy as np  
+from sklearn import linear_model  
+import matplotlib.pyplot as plt  
+  
+x_train = [[2001],[2002],[2003],[2004],[2005],[2006],[2007],[2008],[2009],[2010]]  
+y_train = [[202],[244],[305],[382],[484],[591],[712],[853],[894],[1013]]  
+  
+x_test = [[2011]]  
+  
+linear = linear_model.LinearRegression()  
+linear.fit(x_train,y_train)  
+linear.score(x_train,y_train)  
+  
+print('Coefficient: ',linear.coef_)  
+print('Intercept: ',linear.intercept_)  
+  
+predicted = linear.predict(x_test)  
+print(predicted)  
+  
+plt.figure()  
+plt.scatter(x_train,y_train)  
+plt.plot(x_train,linear.coef_*x_train+linear.intercept_,'r')  
+plt.show()
